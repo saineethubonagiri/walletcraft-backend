e3109c74537162cacab759fc416e45d2c954423c
saineethubonagiri <157181471+saineethubonagiri@users.noreply.github.com>
2025-12-10 16:34:17 -0500

    Updated_files

---
+++ expense-tracker/src/main/java/com/saineethu/expense_tracker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.saineethu.expense_tracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Backend is running!";
+    }
+}
